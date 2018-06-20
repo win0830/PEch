10ceb335fd0f739faca477e31eb83f7a44ebb129
junpei.katsu <junpei.katsu@192.168.150.84>
2018-06-20 17:40:54 +0900

    threads

--- src/main/java/com/example/demo/controller/ArchiveController.java
+++ src/main/java/com/example/demo/controller/ArchiveController.java
@@ -1,7 +1,15 @@
 package com.example.demo.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.model.ThreadsForm;
+import com.google.gson.Gson;
 
 @Controller
 public class ArchiveController {
@@ -9,5 +17,14 @@ public class ArchiveController {
 	@RequestMapping("/archive")
 	public String getArchive() {
 		return "PEch/archive";
+	}
+	
+	@RequestMapping(value = "", 
+			consumes = MediaType.APPLICATION_JSON_VALUE, 
+			method = RequestMethod.GET)
+	@ResponseBody
+	public String getTreads(@RequestBody ThreadsForm fomr) {
+		
+		return new Gson().toJson("");
 	}
 }
