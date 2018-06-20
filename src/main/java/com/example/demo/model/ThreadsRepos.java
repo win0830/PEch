4c0cf480d7951c5f857de4c8e6fd0dce311c694e
junpei.katsu <junpei.katsu@192.168.150.84>
2018-06-20 18:11:29 +0900

    archive controller

--- src/main/java/com/example/demo/model/ThreadsRepos.java
+++ src/main/java/com/example/demo/model/ThreadsRepos.java
@@ -1,7 +1,10 @@
 package com.example.demo.model;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ThreadsRepos extends JpaRepository<Threads, Integer>{
-	Threads findByCategories(Categories categories);
+	List<Threads> findByCategories(Categories categories);
+	List<Threads> findByAllOrderByPostTimeDesc();
 }
