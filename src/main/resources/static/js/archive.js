05eb074b7bdb917ff1c2b3e0f4bda1acf6bf8d71
erika.ogawa <erika.ogawa@192.168.150.76>
2018-06-22 17:22:07 +0900

    archive.jsとthread.htmlの修正

--- src/main/resources/static/js/archive.js
+++ src/main/resources/static/js/archive.js
@@ -1,8 +1,35 @@
 $(function(){
+	
+	/* 
+	* header
+	*/
+	
+	//スレッド新規作成
 	$('#open_create_threads').on('click', function(){
 		$('.create_threads, .create_threads_mask').show();
 	});
 	$('.create_threads_mask').on('click', function(){
 		$('.create_threads, .create_threads_mask').hide();
 	});
+	
+	var params = {
+	        url: "/getThreads",
+	        dataType: "json",
+	        type: "get"
+	    };
+
+	$.ajax(params)
+	.done(function(data,status,jqXHR){
+	    console.log(data);
+	})
+	.fail(function(jqXHR,status,errThrown){
+	    console.error("Error:" + status);
+	});
+	
+	
+	
+	
+	
+	
+	
 });
