1e395f2b6bc2381303ffce7cd71ce3ca7fac59bb
yuki.ohshima <yuki.ohshima@192.168.150.152>
2018-06-25 11:15:34 +0900

    a

--- src/main/java/com/example/demo/controller/ThreadController.java
+++ src/main/java/com/example/demo/controller/ThreadController.java
@@ -1,14 +1,26 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.demo.model.Reses;
+import com.example.demo.model.ResesRepos;
+import com.example.demo.model.ThreadsForm;
 import com.google.gson.Gson;
 
 @Controller
 public class ThreadController {
+	@Autowired
+	private ResesRepos resesRepos;
 
 	@RequestMapping("/thread")
 	public String getTread() {
@@ -20,5 +32,17 @@ public class ThreadController {
 			method = RequestMethod.GET)
 	public String sendMessage() {
 		return new Gson().toJson("");
+	}
+	
+	@RequestMapping(value="/getReses", method = RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+	public String getReses(@RequestBody ThreadsForm threadsForm) {
+		List<Reses> reses = new ArrayList<Reses>();
+		for(Reses res : resesRepos.findAll()) {
+			if(res.getThreads().getThreadId() == threadsForm.getThreadId()) {
+				reses.add(res);
+			}
+		}
+		return new Gson().toJson(reses);
 	}
 }
